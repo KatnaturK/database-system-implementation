0af31d917a6f3e5ebeb589ac035f673c2e94f1d2
Arpan Banerjee <arpanbnrj9@gmail.com>
2020-02-13 00:52:52 -0500

    Two-Pass Multiway Merge Sort Algorithm Phase 1 partially implemented.

--- a2test/BigQ.cc
+++ a2test/BigQ.cc
@@ -1,14 +1,70 @@
+ #include <queue> 
+ #include <algorithm>
+
 #include "BigQ.h"
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
-	// read data from in pipe sort them into runlen pages
+	if(runlen < 1) throw std::runtime_error("BigQ: runlen must be > 0");
 
-    // construct priority queue over sorted runs and dump sorted data 
- 	// into the out pipe
+	this-> in = &in;
+	this-> out = &out;
+	this->runCount = 0;
+	this->runLength = runLength;
+	BigQ::sortOrder = &sortorder;
 
-    // finally shut down the out pipe
+    pthread_t workerThread;
+	int ret = pthread_create(&workerThread, NULL, &worker, (void*) this);
+
 	out.ShutDown ();
 }
+
+void* BigQ :: worker (void *workerThread) {
+	BigQ *bigQWorker = (BigQ*) workerThread;
+	bigQWorker->tppmsPhase1 ();
+
+	delete bigQWorker;
+	pthread_exit(NULL);
+}
+
+bool BigQ :: compareRecords (Record *record1, Record *record2) {
+        ComparisonEngine comparisonEngine;
+        if( comparisonEngine.Compare (record1, record2, BigQ::sortOrder) < 0)
+                return true;
+	   return false;
+}
+
+void BigQ :: tppmsPhase1 () {
+	int recordCount = 0;
+	vector<Record *> records;
+	Record *currRecord = new Record;
+	while (in->Remove (currRecord) && recordCount < runLength) {
+		records.push_back(currRecord);
+		if(recordCount == runLength) {
+			sort (records.begin (), records.end (), compareRecords);
+			generateRuns (records);
+			recordCount = 0;
+			runCount++;
+			records.clear ();
+		}
+	}
+}
+
+void BigQ :: generateRuns (vector<Record *> sortedRecords) {
+	int pageCount = 0;
+	Page currPage;
+	vector<Record *>::iterator currRecord = sortedRecords.begin();
+	for (int i = 0; i < sortedRecords.size (); i++) {
+		if (!currPage.Append (* currRecord)) {
+			runFile.AddPage (&currPage, pageCount++);
+			currPage.EmptyItOut ();
+			currPage.Append (* currRecord);
+		}
+		currRecord++;
+	}
+	runFile.AddPage (&currPage, pageCount++);
+	currPage.EmptyItOut ();
+}
+
 
 BigQ::~BigQ () {
 }
--- a2test/BigQ.h
+++ a2test/BigQ.h
@@ -8,12 +8,26 @@
 
 using namespace std;
 
+
 class BigQ {
-
 public:
-
+	
 	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
 	~BigQ ();
+
+private:
+
+	int runCount, runLength;
+	File runFile;
+	static OrderMaker *sortOrder;
+	Pipe *in, *out;
+
+	static void* worker(void* workerThread);
+	static bool compareRecords(Record *record1, Record *record2);
+
+	void tppmsPhase1 ();
+	void generateRuns (vector<Record *>);
+
 };
 
 #endif
