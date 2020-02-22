a64b7f903facd7b3f8e25e12e78bf21d8d1a7956
KrutantaK <8.katnaturk@gmail.com>
2020-02-22 16:16:53 -0500

    A2test (#2)
    * DBI a2 project initialized.
    
    * Merged DBFile heap changes from a1 to a2test.
    
    * Two-Pass Multiway Merge Sort Algorithm Phase 1 partially implemented.
    
    * Two-Pass Multiway Merge Sort Algorithm Phase 1 implemented.
    
    * TPMMS Algorithm implemented.
    
    * BigQ gtest added.
    
    * Successfully Implemented BigQ Two-Pass Multiway Merge Algorithm.
    
    Co-authored-by: Arpan Banerjee <arpanbnrj9@gmail.com>

---
+++ a2test/DBFile.h
@@ -0,0 +1,49 @@
+#ifndef DBFILE_H
+#define DBFILE_H
+
+#include "TwoWayList.h"
+#include "Record.h"
+#include "Schema.h"
+#include "File.h"
+#include "Comparison.h"
+#include "ComparisonEngine.h"
+
+typedef enum {heap, sorted, tree} fType;
+
+using namespace std;
+
+// stub DBFile header..replace it with your own DBFile.h 
+
+class DBFile {
+    char *filename;
+    File *heapfile;
+    Page *deltapage;
+    Page *readpage;
+    
+    ComparisonEngine comp;
+
+    int readPageNumber;
+
+    // adds the page holding recent changes/transactions
+    void AddDeltaPageToFile();
+
+public:
+	DBFile ();
+    ~DBFile ();
+
+	int Create (const char *fpath, fType file_type, void *startup);
+	int Open (const char *fpath);
+	int Close ();
+
+	void Load (Schema &myschema, const char *loadpath);
+
+	void MoveFirst ();
+	void Add (Record &addme);
+	int GetNext (Record &fetchme);
+	int GetNext (Record &fetchme, CNF &cnf, Record &literal);
+
+private:
+	bool fileExists(const char *filePath);
+
+};
+#endif
---
+++ a2test/gtestScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./bigQTest.out < gtc1.txt
