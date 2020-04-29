ced2037abb5d0acaf86163b955cec6cf4896b08e
KrutantaK <krutantakb.patil@ufl.edu>
2020-04-28 23:09:37 -0400

    Final project Initialized.

---
+++ a5/Interpreter.cc
@@ -0,0 +1,110 @@
+#include <iostream>
+
+#include "Errors.h"
+#include "Interpreter.h"
+#include "ParseTree.h"
+#include "Statistics.h"
+#include "QueryPlan.h"
+#include "Ddl.h"
+
+using std::cout;
+
+extern "C" {
+  int yyparse(void);     // defined in y.tab.c
+}
+
+extern struct FuncOperator *finalFunction; // the aggregate function (NULL if no agg)
+extern struct TableList *tables; // the list of tables and aliases in the query
+extern struct AndList *boolean; // the predicate in the WHERE clause
+extern struct NameList *groupingAtts; // grouping atts (NULL if no grouping)
+extern struct NameList *attsToSelect; // the set of attributes in the SELECT (NULL if no such atts)
+extern int distinctAtts; // 1 if there is a DISTINCT in a non-aggregate query 
+extern int distinctFunc;  // 1 if there is a DISTINCT in an aggregate query
+extern char* newtable;
+extern char* oldtable;
+extern char* newfile;
+extern char* deoutput;
+extern struct AttrList *newattrs; //Use this to build Attribute* array and record schema
+
+void Interpreter::run() {
+  char *fileName = "Statistics.txt";
+  Statistics s;
+  Ddl d; 
+  QueryPlan plan(&s);
+  int choice ;
+  bool connected = false;
+
+  while(true) {
+    cout << "\n1: Fire up the Database" << endl;
+    cout << "2: Query Database" << endl;
+    cout << "3: Close Database" << endl;
+    cout << "Enter Option:          $";
+    cin >> choice;
+
+    switch(choice) {
+      case 1: 
+          if(connected) {
+            cout << "\n--- Database Already Connected ---\n" << endl;
+            continue;
+          }
+          cout << "\n--- Database Connection Established ---\n" << endl;
+	        connected = true;
+          break;
+
+      case 2:
+          if(!connected) {
+            cout << "\n--- Connection Not Established Yet---\n" << endl;
+            continue;
+          }
+          cout << "\nSET OUTPUT <STDOUT(default)>, <'file_name'>, <NONE> and Enter Query CNF(enter ';' at the end of the query).\n" << endl;
+          cout << "\n" << endl;
+          if (yyparse() != 0) {
+            cout << "\nCan't parse your CNF.\n";
+            continue;
+          }
+
+        s.Read(fileName);
+
+        if (newtable) {
+          if (d.createTable()) cout << "Create table " << newtable << std::endl;
+          else cout << "Table " << newtable << " already exists." << std::endl;
+        } else if (oldtable && newfile) {
+          if (d.insertInto()) cout << "Insert into " << oldtable << std::endl;
+          else cout << "Insert failed." << std::endl;
+        } else if (oldtable && !newfile) {
+          if (d.dropTable()) cout << "Drop table " << oldtable << std::endl;
+          else cout << "Table " << oldtable << " does not exist." << std::endl;
+        } else if (deoutput) {
+          plan.setOutput(deoutput);
+        } else if (attsToSelect || finalFunction) {
+          plan.plan();
+          plan.print();
+          plan.execute();
+        }
+        clear();
+        break;
+
+      case 3:
+          if(!connected)
+            cout<<"\n--- Connection Not Established Yet---\n"<<endl;
+	        else {
+            cout<< "\n--- Database Shutdown ---\n"<<endl;
+	          exit(0);
+          }
+          break;
+    }
+  }
+}
+
+// TODO: free lists
+void Interpreter::clear() {
+  newattrs = NULL;
+  finalFunction = NULL;
+  tables = NULL;
+  boolean = NULL;
+  groupingAtts = NULL;
+  attsToSelect = NULL;
+  newtable = oldtable = newfile = deoutput = NULL;
+  distinctAtts = distinctFunc = 0;
+  FATALIF (!remove ("*.tmp"), "remove tmp files failed");
+}
---
+++ a5/gtest.cc
@@ -0,0 +1,64 @@
+#include "gtest/gtest.h"
+#include <iostream>
+#include <cstring>
+#include <climits>
+#include <string>
+#include <algorithm>
+
+#include "Errors.h"
+#include "Interpreter.h"
+#include "ParseTree.h"
+#include "QueryPlan.h"
+#include "Statistics.h"
+#include "Ddl.h"
+
+extern "C" {
+  int yyparse(void);     // defined in y.tab.c
+  struct YY_BUFFER_STATE* yy_scan_string(const char*);
+}
+
+extern struct FuncOperator *finalFunction; // the aggregate function (NULL if no agg)
+extern struct TableList *tables; // the list of tables and aliases in the query
+extern struct AndList *boolean; // the predicate in the WHERE clause
+extern struct NameList *groupingAtts; // grouping atts (NULL if no grouping)
+extern struct NameList *attsToSelect; // the set of attributes in the SELECT (NULL if no such atts)
+extern int distinctAtts; // 1 if there is a DISTINCT in a non-aggregate query
+extern int distinctFunc;  // 1 if there is a DISTINCT in an aggregate query
+extern char* newtable;
+extern char* oldtable;
+extern char* newfile;
+extern char* deoutput;
+extern struct AttrList *newattrs; //Use this to build Attribute* array and record schema
+
+using namespace std;
+
+char* catalog_path = "catalog";
+char* dbfile_dir = "bin/";
+char* tpch_dir = "../DATA/1G";
+
+char *fileName = "Statistics.txt";
+Statistics stats;
+Ddl dataDef;
+QueryPlan plan(&stats);
+
+
+TEST (DATABASE, CREATE_TABLE) {
+	char* cnf = "CREATE TABLE gtestTable(attribute_value INTEGER, attribute_value STRING) AS HEAP;";
+	yy_scan_string(cnf);
+	yyparse();
+	stats.Read(fileName);
+	ASSERT_EQ(true, dataDef.createTable());
+}
+
+TEST (DATABASE, DROP_TABLE) {
+	char* cnf = "DROP TABLE gtestTable;";
+	yy_scan_string(cnf);
+	yyparse();
+	stats.Read(fileName);
+	ASSERT_EQ(true, dataDef.dropTable());
+}
+
+int main(int argc, char *argv[]) {
+	testing::InitGoogleTest(&argc, argv); 
+	return RUN_ALL_TESTS ();
+}
